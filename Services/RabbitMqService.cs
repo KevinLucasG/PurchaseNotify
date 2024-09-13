9c4343244d8d5023ce7bdc06dd9297223011544c
Kevin <kevin.lucassg@gmail.com>
2024-09-12 22:16:16 -0300

    Added RabbitMqService

---
+++ Services/RabbitMqService.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+
+namespace PurchaseNotify.Services
+{
+    public class RabbitMqService
+    {
+        private readonly string _hostName;
+        private readonly string _queueName;
+
+        public RabbitMqService(string hostName, string queueName)
+        {
+            _hostName = hostName;
+            _queueName = queueName;
+        }
+
+        public void SendMessage(byte[] message)
+        {
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: _queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: _queueName,
+                                     basicProperties: properties,
+                                     body: message);
+            }
+        }
+    }
+
+}
